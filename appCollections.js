652412326b234ee83945c34544338d0e24e86ca2
Harry Ward <harryward@townsquaremedia.com>
2015-08-26 14:00:48 -0600

    image Size

--- appCollections.js
+++ appCollections.js
@@ -5,7 +5,7 @@ Events = new Mongo.Collection("Events")
 // var thumbStore = new FS.Store.GridFS("thumb");
 var thumbStore = new FS.Store.GridFS("thumb", {
    transformWrite: function(fileObj, readStream, writeStream) {
-       gm(readStream, fileObj.name()).autoOrient().resize('400', '250^').stream().pipe(writeStream);
+       gm(readStream, fileObj.name()).autoOrient().resize('600', '250^').stream().pipe(writeStream);
    }
 });
 
