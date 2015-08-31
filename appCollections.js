82c7f2fb8168ba02c017dc3b0043074f16a47b40
Harry Ward <harryward@townsquaremedia.com>
2015-08-31 11:32:16 -0600

    test

--- appCollections.js
+++ appCollections.js
@@ -2,8 +2,9 @@ Events = new Mongo.Collection("Events")
 
 var thumbStore = new FS.Store.GridFS("pictures", {
    transformWrite: function(fileObj, readStream, writeStream) {
-       gm(readStream, fileObj.name()).autoOrient().resize('600', '250^').stream().pipe(writeStream);
+    this.gm(readStream, fileObj.name).resize('600', '250^').stream().pipe(writeStream);
    }
+
 });
 
 Images = new FS.Collection("images", {
